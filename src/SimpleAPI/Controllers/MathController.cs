8d317576a7613139146729e69f6a8f592bef6eca
CircleQMinh <quocmimn.vutran3105@gmail.com>
2023-08-05 13:52:53 +0700

    Init Project

---
+++ src/SimpleAPI/Controllers/MathController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet("AddTwoNumber")]
+        public ActionResult<int> AddTwoNumber(int numberOne,int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        [HttpGet("SubtractTwoNumber")]
+        public ActionResult<int> SubtractTwoNumber(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+    }
+}
---
+++ test/SimpleAPI.Test/MathControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using SimpleAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAPI.Test
+{
+    public class MathControllerTests
+    {
+        MathController controller = new MathController();
+        [Fact]
+        public void MathController_AddTwoNumber_ValidInput()
+        {
+            //arrange
+            int numberOne = 4;
+            int numberTwo = 5;
+            //act
+            var result = controller.AddTwoNumber(numberOne, numberTwo).Value;
+            //assert
+            Assert.Equal(9,result);
+        }
+
+        [Fact]
+        public void MathController_SubtractTwoNumber_ValidInput()
+        {
+            //arrange
+            int numberOne = 4;
+            int numberTwo = 5;
+            //act
+            var result = controller.SubtractTwoNumber(numberOne, numberTwo).Value;
+            //assert
+            Assert.Equal(-1, result);
+        }
+    }
+}
