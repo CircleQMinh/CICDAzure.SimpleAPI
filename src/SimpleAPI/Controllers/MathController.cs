b9637574d3b39a8549d34fa85ebd27492e786785
CircleQMinh <quocmimn.vutran3105@gmail.com>
2023-08-05 15:53:37 +0700

    Trigger build

--- src/SimpleAPI/Controllers/MathController.cs
+++ src/SimpleAPI/Controllers/MathController.cs
@@ -16,6 +16,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("SubtractTwoNumber")]
         public ActionResult<int> SubtractTwoNumber(int numberOne, int numberTwo)
         {
+            //trigger build
             return numberOne - numberTwo;
         }
     }
